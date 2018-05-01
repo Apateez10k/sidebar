98eab0bcbceb6637e72f3092453718fd3f949b90
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-01 15:29:56 -0700

    Add data generation file

---
+++ database/data-generation.js
@@ -0,0 +1,22 @@
+var faker = require('faker');
+
+{ id: 'ChIJFUBxSY6AhYARwOaLV7TsLjw',
+  name: 'Clift San Francisco',
+  menu_url: 'http://google.com',
+  address: '495 Geary St, San Francisco, CA 94102, USA',
+  location: 'https://maps.google.com/?cid=4336663750511421120',
+  url: 'https://www.morganshotelgroup.com/originals/originals-clift-san-francisco?utm_source=Google%20My%20Business&utm_medium=Website%20Button&utm_campaign=San%20Francisco',
+  phone: '+1 415-775-4700',
+  hours: 
+    [ 'Monday: 8:00 AM – 6:00 PM',
+     'Tuesday: 8:00 AM – 6:00 PM',
+     'Wednesday: 8:00 AM – 6:00 PM',
+     'Thursday: 8:00 AM – 6:00 PM',
+     'Friday: 8:00 AM – 6:00 PM',
+     'Saturday: 8:00 AM – 6:00 PM',
+     'Sunday: 8:00 AM – 6:00 PM' 
+    ],
+  coords: { lat: 37.7867167, lng: -122.4111737 } 
+ }
+
+ 
