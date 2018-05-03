bd143586986262f3e75d6594acf75e3c6a61a342
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-03 15:39:41 -0700

    Lint data-generation.js

--- database/data-generation.js
+++ database/data-generation.js
@@ -1,51 +1,44 @@
-var faker = require('faker');
-var fs = require('file-system');
+const faker = require('faker');
+const fs = require('file-system');
 
-var output = {};
+let output = {};
 
-var writableStream = fs.createWriteStream('seed-data.json', {'flags': 'a'});
+const writableStream = fs.createWriteStream('seed-data.json', { flags: 'a' });
 
 
-var generateData = function(numItems, idStart) {
-	for (var i = 0; i < numItems; i++) {
-		output = {}
+const generateData = function (numItems, idStart) {
+  for (let i = 0; i < numItems; i++) {
+    output = {};
 
-		if (i % 250000 === 0) {
-			console.log(i);
-		}
+    if (i % 250000 === 0) {
+      console.log(i);
+    }
 
-		output.id = idStart + i;
-		output.menu_url = 'http://google.com';
-		output.address = faker.fake("{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA");
-		output.location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
-		output.url = faker.internet.url();
-		output.phone = faker.fake("+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}");
-		output.hours = [ 'Monday: 8:00 AM – 6:00 PM',
-		     'Tuesday: 8:00 AM – 6:00 PM',
-		     'Wednesday: 8:00 AM – 6:00 PM',
-		     'Thursday: 8:00 AM – 6:00 PM',
-		     'Friday: 8:00 AM – 6:00 PM',
-		     'Saturday: 8:00 AM – 6:00 PM',
-		     'Sunday: 8:00 AM – 6:00 PM' 
-		    ];
-		output.coords = {
-			lat: faker.random.number(180) - 90,
-			lng: faker.random.number(360) - 180,
-		}
+    output.id = idStart + i;
+    output.menu_url = 'http://google.com';
+    output.address = faker.fake('{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA');
+    output.location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
+    output.url = faker.internet.url();
+    output.phone = faker.fake('+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}');
+    output.hours = ['Monday: 8:00 AM – 6:00 PM', 'Tuesday: 8:00 AM – 6:00 PM', 'Wednesday: 8:00 AM – 6:00 PM', 'Thursday: 8:00 AM – 6:00 PM', 'Friday: 8:00 AM – 6:00 PM', 'Saturday: 8:00 AM – 6:00 PM', 'Sunday: 8:00 AM – 6:00 PM'];
+    output.coords = {
+      lat: faker.random.number(180) - 90,
+      lng: faker.random.number(360) - 180,
+    };
 
-		if (i === 0) {
-			writableStream.write(JSON.stringify(output) + ', \n');
-		} else if (i === numItems - 1){
-			writableStream.write(JSON.stringify(output) + ', \n]');
-		} else {
-			writableStream.write(JSON.stringify(output) + ', \n');				
-		}
-	}
-}
+    if (i === 0) {
+      writableStream.write(`${JSON.stringify(output)}, \n`);
+    } else if (i === numItems - 1) {
+      writableStream.write(`${JSON.stringify(output)}, \n]`);
+    } else {
+      writableStream.write(`${JSON.stringify(output)}, \n`);
+    }
+  }
+};
 
 generateData(5100000, 5000001);
 
-//Below is the expected layout of the data
+// Below is the expected layout of the data
 
 // var test = { id: 'ChIJFUBxSY6AhYARwOaLV7TsLjw',
 //   name: 'Clift San Francisco',
@@ -54,14 +47,14 @@ generateData(5100000, 5000001);
 //   location: 'https://maps.gdoogle.com/?cid=4336663750511421120',
 //   url: 'https://www.morganshotelgroup.com/originals/originals-clift-san-francisco?utm_source=Google%20My%20Business&utm_medium=Website%20Button&utm_campaign=San%20Francisco',
 //   phone: '+1 415-775-4700',
-//   hours: 
+//   hours:
 //     [ 'Monday: 8:00 AM – 6:00 PM',
 //      'Tuesday: 8:00 AM – 6:00 PM',
 //      'Wednesday: 8:00 AM – 6:00 PM',
 //      'Thursday: 8:00 AM – 6:00 PM',
 //      'Friday: 8:00 AM – 6:00 PM',
 //      'Saturday: 8:00 AM – 6:00 PM',
-//      'Sunday: 8:00 AM – 6:00 PM' 
+//      'Sunday: 8:00 AM – 6:00 PM'
 //     ],
-//   coords: { lat: 37.7867167, lng: -122.4111737 } 
+//   coords: { lat: 37.7867167, lng: -122.4111737 }
 //  };
