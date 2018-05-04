c9a32349ae60ba0238af7e83c777ac8188af3481
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-04 16:14:21 -0700

    Add test files for data generation

---
+++ tests/__tests__/data-generation.test.js
@@ -0,0 +1,5 @@
+const generate = require('../../database/data-generation.js');
+
+test('generateData should return when it has written the appropriate number of items to the specified file', () => {
+	expect(generate.generateData(10, 0, 'test.json')).toBe('Finished Writing Data');
+});
