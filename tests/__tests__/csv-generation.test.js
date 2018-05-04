5451648327fcfa881e3b4d422ab0cc50cd8873c9
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-04 16:30:38 -0700

    Change test name

--- tests/__tests__/csv-generation.test.js
+++ tests/__tests__/csv-generation.test.js
@@ -1,5 +1,5 @@
 const generate = require('../../database/data-generation.js');
 
-test('generateData should return when it has written the appropriate number of items to the specified file', () => {
+test('the generateData function should return when it has written the appropriate number of items to the specified file', () => {
 	expect(generate.generateData(10, 0, 'test.json')).toBe('Finished Writing Data');
 });
