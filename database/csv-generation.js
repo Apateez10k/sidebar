4f85b7e80e8c304921075b7b2065b7ca64667f6a
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-05 14:23:47 -0700

    Refactor for testing

--- database/csv-generation.js
+++ database/csv-generation.js
@@ -1,6 +1,18 @@
 const faker = require('faker');
 const fs = require('file-system');
 
+const generateItem = function (idStart, i) {
+  const id = idStart + i;
+  const menu_url = 'http://google.com';
+  const address = faker.fake('{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA');
+  const location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
+  const url = faker.internet.url();
+  const phone = faker.fake('+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}');
+  const hours = '{Monday: 8:00 AM – 6:00 PM, Tuesday: 8:00 AM – 6:00 PM, Wednesday: 8:00 AM – 6:00 PM, Thursday: 8:00 AM – 6:00 PM, Friday: 8:00 AM – 6:00 PM, Saturday: 8:00 AM – 6:00 PM, Sunday: 8:00 AM – 6:00 PM}';
+  const coords = `{${(faker.random.number(180) - 90).toString()}, ${(faker.random.number(360) - 180).toString()}}`;
+
+  return `${id.toString()}|${menu_url}|${address}|${location}|${url}|${phone}|${hours}|${coords}\n`;
+};
 
 const generateData = function (numItems, idStart, path) {
   const writableStream = fs.createWriteStream(path, { flags: 'a' });
@@ -10,16 +22,9 @@ const generateData = function (numItems, idStart, path) {
       console.log(i);
     }
 
-    const id = idStart + i;
-    const menu_url = 'http://google.com';
-    const address = faker.fake('{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA');
-    const location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
-    const url = faker.internet.url();
-    const phone = faker.fake('+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}');
-    const hours = '{Monday: 8:00 AM – 6:00 PM, Tuesday: 8:00 AM – 6:00 PM, Wednesday: 8:00 AM – 6:00 PM, Thursday: 8:00 AM – 6:00 PM, Friday: 8:00 AM – 6:00 PM, Saturday: 8:00 AM – 6:00 PM, Sunday: 8:00 AM – 6:00 PM}';
-    const coords = `{${(faker.random.number(180) - 90).toString()}, ${(faker.random.number(360) - 180).toString()}}`;
+    const data = generateItem(idStart, i);
 
-    writableStream.write(`${id.toString()}|${menu_url}|${address}|${location}|${url}|${phone}|${hours}|${coords}\n`);
+    writableStream.write(data);
     if (i === numItems - 1) {
       return ('Finished Writing Data');
     }
--- database/data-generation.js
+++ database/data-generation.js
@@ -3,36 +3,40 @@ const fs = require('file-system');
 
 let output = {};
 
+const generateItem = function (idStart, i) {
+  output = {};
+
+  output.id = idStart + i;
+  output.menu_url = 'http://google.com';
+  output.address = faker.fake('{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA');
+  output.location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
+  output.url = faker.internet.url();
+  output.phone = faker.fake('+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}');
+  output.hours = ['Monday: 8:00 AM – 6:00 PM', 'Tuesday: 8:00 AM – 6:00 PM', 'Wednesday: 8:00 AM – 6:00 PM', 'Thursday: 8:00 AM – 6:00 PM', 'Friday: 8:00 AM – 6:00 PM', 'Saturday: 8:00 AM – 6:00 PM', 'Sunday: 8:00 AM – 6:00 PM'];
+  output.coords = {
+    lat: faker.random.number(180) - 90,
+    lng: faker.random.number(360) - 180,
+  };
+
+  return output;
+};
+
 const generateData = function (numItems, idStart, file) {
-  
-const writableStream = fs.createWriteStream(file, { flags: 'a' });
+  const writableStream = fs.createWriteStream(file, { flags: 'a' });
 
   for (let i = 0; i < numItems; i++) {
-
-    output = {};
-
     if (i % 250000 === 0) {
       console.log(i);
     }
 
-    output.id = idStart + i;
-    output.menu_url = 'http://google.com';
-    output.address = faker.fake('{{address.streetAddress}} {{address.streetName}}, San Francisco, CA 94016, USA');
-    output.location = 'https://maps.gdoogle.com/?cid=4336663750511421120';
-    output.url = faker.internet.url();
-    output.phone = faker.fake('+1 {{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}-{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}{{random.number(9)}}');
-    output.hours = ['Monday: 8:00 AM – 6:00 PM', 'Tuesday: 8:00 AM – 6:00 PM', 'Wednesday: 8:00 AM – 6:00 PM', 'Thursday: 8:00 AM – 6:00 PM', 'Friday: 8:00 AM – 6:00 PM', 'Saturday: 8:00 AM – 6:00 PM', 'Sunday: 8:00 AM – 6:00 PM'];
-    output.coords = {
-      lat: faker.random.number(180) - 90,
-      lng: faker.random.number(360) - 180,
-    };
+    const data = generateItem(idStart, i);
 
     if (i === 0) {
-      writableStream.write(`${JSON.stringify(output)}, \n`);
+      writableStream.write(`${JSON.stringify(data)}, \n`);
     } else if (i === numItems - 1) {
-      writableStream.write(`${JSON.stringify(output)}, \n]`);
+      writableStream.write(`${JSON.stringify(data)}, \n]`);
     } else {
-      writableStream.write(`${JSON.stringify(output)}, \n`);
+      writableStream.write(`${JSON.stringify(data)}, \n`);
     }
 
     if (i === numItems - 1) {
@@ -43,5 +47,5 @@ const writableStream = fs.createWriteStream(file, { flags: 'a' });
 
 exports.generateData = generateData;
 
-//Commented out for testing
+// Commented out for testing
 // generateData(5100000, 5000001, 'seed-data.json');
