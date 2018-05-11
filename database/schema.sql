08010106c6a89e304fde902d292e7fdd5746253b
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-11 16:00:41 -0700

    Add postgres schema and script to connect to database

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE sidebar;
+
+USE photos;
+
+CREATE TABLE places (
+	id INT,
+	name TEXT,
+	menu_url TEXT,
+	address TEXT,
+	location TEXT,
+	url TEXT,
+	phone TEXT,
+	hours TEXT ARRAY,
+	coords TEXT ARRAY
+)
