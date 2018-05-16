2e4763d5aee5cfbd2cdb0ae12ea644d849a5ac07
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-16 15:22:10 -0700

    Add handler for sending css to proxy server

--- server/server.js
+++ server/server.js
@@ -2,47 +2,46 @@ const newRelic = require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 1337;
 const bodyParser = require('body-parser');
-const Places = require('../database/index.js');
 const db = require('../database/index.js');
-// const db = require('../database/postgres.js');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
 app.use('/restaurants', express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/restaurants/:id', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-})
+app.get('/restaurants/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
-//MongoDB
-app.get('/api/restaurants/:id', function(req, res) {
-  let q = db.findOne({id: parseInt(req.params.id)});
+app.get('/app.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/services/app.js'));
+});
+
+app.get('/app-server.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/services/app-server.js'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/style.css'));
+});
+
+app.get('/api/restaurants/:id', (req, res) => {
+  const q = db.findOne({ id: parseInt(req.params.id) });
 
   q.exec((err, place) => {
-    if (err) { console.log(err) }
-    console.log('PLACE: ', place)
+    if (err) { console.log(err); }
     res.send(place);
   });
 });
-
-//PostgreSQL
-// app.get('/api/restaurants/:id', (req, res) => {
-//   db.findOne(req.params.id)
-//     .then((data) => {
-//       res.send(data);
-//     });
-// });
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
