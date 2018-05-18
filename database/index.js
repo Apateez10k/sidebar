1bd486c0ada6a974c4a3e3428c7223922e4d8f5b
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-18 09:20:11 -0700

    Add function to query redis cache

--- database/index.js
+++ database/index.js
@@ -1,3 +1,6 @@
+var redisClient = require('redis').createClient;
+var redis = redisClient(6379, 'localhost');
+
 const mongoose = require('mongoose');
 const mongoUrlDocker = 'mongodb://database/sidebar';
 const mongoUrl = 'mongodb://localhost/sidebar';
@@ -37,6 +40,28 @@ const clearDb = (cb) => {
 }
 
 const db = mongoose.connect(mongoUrl);
+
+module.exports.findCached = function (db, redis, id, callback) {
+    redis.get(id, function (err, reply) {
+        if (err) {
+          callback(null);
+        } else if (reply) {
+          //Book exists in cache
+          callback(JSON.parse(reply));
+        } else {
+            //Book doesn't exist in cache - we need to query the main database
+            db.collection('places').findOne({ id: id }, function (err, data) {
+                if (err || !doc) {
+                  callback(null);
+                } else {
+                    redis.set(id, JSON.stringify(data), function () {
+                        callback(data);
+                    });
+                }
+            });
+        }
+    });
+};
 
 module.exports = db;
 module.exports = Places;
