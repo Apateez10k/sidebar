67b74401c207c8e5bc3ca9655107e528f1e0ead1
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-05-18 11:44:24 -0700

    Configure redis

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,3 +3,4 @@ import ReactDOM from 'react-dom';
 import Sidebar from './components/Sidebar.jsx';
 
 export default Sidebar;
+ReactDOM.render(<Sidebar name="sidebar"/>, document.getElementById('Sidebar'));
